b7a16f71d6692105ceb8d653ae4640a5664005d0
Vyacheslavna <k.bronskaya@gmail.com>
2016-03-07 22:31:00 +0700

    Added files via upload
    code optimization

--- .idea/bin/spec/VISA_spec.rb
+++ .idea/bin/spec/VISA_spec.rb
@@ -2,56 +2,55 @@ require 'rspec'
 require 'rails_helper'
 require 'airborne'
 require 'mongo'
-require '../.idea/payment.rb'
 
 include Mongo
+
+Airborne.configure do |config|
+  config.base_url = 'http://kira-35411.herokuapp.com/api'
+end
 
 describe 'VISA' do
   before (:all) {
     @client_hq = Mongo::Client.new(['ds059155.mongolab.com:59155'], :database => 'heroku_9zz9bm0g', :user => 'heroku_9zz9bm0g', :password => '9qtdvpu61se7usccbh2n3jd103')
-    db_hq = @client_hq.database
-    @coll_hq = db_hq.collection(db_hq)
+    db_hq = @client_hq.database()
+    #db_hq.collection_names
+    #returns D, [2016-03-07T21:29:04.020019 #8220] DEBUG -- : MONGODB | ds059155.mongolab.com:59155 | heroku_9zz9bm0g.listCollections | STARTED | {"listCollections"=>1, "cursor"=>{}, "filter"=>{:name=>{"$not"=>/system.|\$/}}}
+    #@coll_hq = db_hq.collection("system.")
   }
   after (:all) {
     @client_hq.close
   }
 
   it('should fill the fields with EUR') {
-    #post 'http://kira-35411.herokuapp.com/api/payment', {:amount => 12, :currency => 'EUR', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => 123, :expireMonth => 3, :expireYear => 2021}
-    Payment.postPayment('12', 'EUR', 'John Smith', 'John', 'Smith', '4032039575052396', '123', '3', '2021')
+    post '/payment', {:amount => '12', :currency => 'EUR', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => '123', :expireMonth => '3', :expireYear => '2021'}
     expect_status(201)
 
-   # gateway_check = :string
-
-   # while gateway_check != 'paypal'
-      #gateway_check = coll_hq.find().each { |row| puts row.enumerator }.next
-     # puts (gateway_check)
-    #end
+    #expect(@coll_hq).to_not be_nil
+    #expect(@coll_hq).not_to be_empty
   }
 
   it('should fill the fields with USD') {
-    post 'http://kira-35411.herokuapp.com/api/payment', {:amount => 12, :currency => 'USD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => 123, :expireMonth => 3, :expireYear => 2021}
+    post '/payment', {:amount => '12', :currency => 'USD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => '123', :expireMonth => '3', :expireYear => '2021'}
     expect_status(201)
   }
 
   it('should fill the fields with AUD') {
-    post 'http://kira-35411.herokuapp.com/api/payment', {:amount => 12, :currency => 'AUD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => 123, :expireMonth => 3, :expireYear => 2021}
+    post '/payment', {:amount => '12', :currency => 'AUD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => '123', :expireMonth => '3', :expireYear => '2021'}
     expect_status(201)
   }
 
   it('should fill the fields with THB') {
-    post 'http://kira-35411.herokuapp.com/api/payment', {:amount => 12, :currency => 'THB', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => 123, :expireMonth => 3, :expireYear => 2021}
+    post '/payment', {:amount => '12', :currency => 'THB', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => '123', :expireMonth => '3', :expireYear => '2021'}
     expect_status(201)
   }
-=begin
+
   it('should fill the fields with HKD') {
-    post 'http://kira-35411.herokuapp.com/api/payment', {:amount => 12, :currency => 'HKD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => 123, :expireMonth => 3, :expireYear => 2021}
+    post '/payment', {:amount => '12', :currency => 'HKD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => '123', :expireMonth => '3', :expireYear => 2021}
     expect_status(201)
   }
 
   it('should fill the fields with SGD') {
-    post 'http://kira-35411.herokuapp.com/api/payment', {:amount => 12, :currency => 'SGD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => 123, :expireMonth => 3, :expireYear => 2021}
+    post '/payment', {:amount => '12', :currency => 'SGD', :fullName => 'John Smith', :firstName => 'John', :lastName => 'Smith', :cardNumber => '4032039575052396', :cardCVV => '123', :expireMonth => '3', :expireYear => '2021'}
     expect_status(201)
   }
-=end
 end
